9b74c671ac9889697bfb7dc437fe87c56695a212
viraj159 <virajchoksi276@gmail.com>
2025-05-05 21:53:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,41 +3,30 @@ from heapq import heappush, heappop
 from math import sqrt
 
 def prim(graph):
-    """
-    ### TODO:
-    Update this method to work when the graph has multiple connected components.
-    Rather than returning a single tree, return a list of trees,
-    one per component, containing the MST for each component.
-
-    Each tree is a set of (weight, node1, node2) tuples.    
-    """
-    def prim_helper(visited, frontier, tree):
-        if len(frontier) == 0:
-            return tree
-        else:
+    def prim_helper(start_node):
+        frontier = []
+        heappush(frontier, (0, start_node, start_node))
+        tree = set()
+        while frontier:
             weight, node, parent = heappop(frontier)
             if node in visited:
-                return prim_helper(visited, frontier, tree)
-            else:
-                print('visiting', node)
-                # record this edge in the tree
-                tree.add((weight, node, parent))
-                visited.add(node)
-                for neighbor, w in graph[node]:
-                    heappush(frontier, (w, neighbor, node))    
-                    # compare with dijkstra:
-                    # heappush(frontier, (distance + weight, neighbor))                
+                continue
+            visited.add(node)
+            tree.add((weight, node, parent))
+            for neighbor, w in graph[node]:
+                if neighbor not in visited:
+                    heappush(frontier, (w, neighbor, node))
+        return tree
 
-                return prim_helper(visited, frontier, tree)
-        
-    # pick first node as source arbitrarily
-    source = list(graph.keys())[0]
-    frontier = []
-    heappush(frontier, (0, source, source))
-    visited = set()  # store the visited nodes (don't need distance anymore)
-    tree = set()
-    prim_helper(visited, frontier, tree)
-    return tree
+    visited = set()
+    forest = []
+    for node in graph:
+        if node not in visited:
+            tree = prim_helper(node)
+            # Remove the initial dummy edge (0, node, node)
+            tree.discard((0, node, node))
+            forest.append(tree)
+    return forest
 
 def test_prim():    
     graph = {
